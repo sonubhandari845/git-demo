18f220e062e62643429a0fbfaaa54191256788f4
Sonu Bhandari <sonubhandari@Sonus-MacBook-Pro.local>
2022-02-12 20:08:08 +0530

    Added fn headers

--- math.py
+++ math.py
@@ -1,11 +1,15 @@
+# Add implementation
 def add(x,y):
 	pass
 
+# Substract implementation
 def substract(x,y):
 	pass
 
+# Multiply implementation
 def multiply(x,y):
 	pass
 
+# Divide implementation
 def divide(x,y):
 	pass
